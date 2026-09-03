394abf14cf266dcf0f83291ce71f7d7cf6d85e08
just_dinfa <dinfabincan@gmail.com>
2026-09-03 03:47:04 -0800

    Variables

--- assignment 2/Assignment 2- profile.py
+++ assignment 2/Assignment 2- profile.py
@@ -1,4 +1,4 @@
-#STUDENT INFORMATION CARD
+STUDENT INFORMATION CARD
 full_name="John Doe"
 age="20"
 department="computer science"
@@ -11,9 +11,9 @@ birth_year=2005
 current_year=2026
 calculated_age= current_year-birth_year
 
-print("---")
+print("--------------------------")
 print("STUDENT INFORMATION CARD")
-print("---")
+print("--------------------------")
 print("full name:", full_name)
 print("age:", age)
 print("department:", department)
