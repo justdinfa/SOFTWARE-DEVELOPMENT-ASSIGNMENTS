aecb38188a6a15e6f79535a235a04ffffea9a2d2
just_dinfa <dinfabincan@gmail.com>
2026-09-03 04:25:35 -0800

    ATM Machine

--- Assignment 6/assignment 6- python.py
+++ Assignment 6/assignment 6- python.py
@@ -6,9 +6,22 @@ print("2. Deposit")
 print("3. Withdraw")
 print("4. Exit")
 
-balance=100000
-choice=input("Enter your choice (1-4): ")
-if choice=="1":
+balance = 100000
+choice = input("Enter your choice (1-4): ")
+if choice == "1":
     print("Your balance is:", balance)
-elif choice=="2":
-    
+elif choice == "2":
+    amount = float(input("Enter amount to deposit: "))
+    balance += amount
+    print("Deposit successful. New balance is:", balance)
+elif choice == "3":
+    amount = float(input("Enter amount to withdraw: "))
+    if amount <= balance:
+        balance -= amount
+        print("Withdrawal successful. New balance is:", balance)
+    else:
+        print("Insufficient funds.")
+elif choice == "4":
+    print("Thank you for using our services.")
+else:
+    print("Invalid choice. please select a valid option (1-4).")
